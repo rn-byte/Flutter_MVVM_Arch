a70e42b84367e0cb453868f4187e963739104155
rn-byte <ramthakur977+github@gmail.com>
2024-06-30 18:34:15 +0545

    some function in utils class added

---
+++ lib/utils/utils.dart
@@ -0,0 +1,43 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+toastMessage(String message) {
+  Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_LONG,
+      backgroundColor: Colors.black,
+      textColor: Colors.white);
+}
+
+void flushErrorMessage(String message, BuildContext context) {
+  showFlushbar(
+      context: context,
+      flushbar: Flushbar(
+        message: message,
+        forwardAnimationCurve: Curves.decelerate,
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: const EdgeInsets.all(15),
+        duration: const Duration(seconds: 3),
+        backgroundColor: Colors.red,
+        reverseAnimationCurve: Curves.easeInOut,
+        positionOffset: 20,
+        borderRadius: BorderRadius.circular(20),
+        flushbarPosition: FlushbarPosition.TOP,
+        icon: const Icon(
+          Icons.error,
+          size: 29,
+          color: Colors.white,
+        ),
+      )..show(context));
+}
+
+void showSnackbar(String message, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      backgroundColor: Colors.red,
+      content: Text(message),
+    ),
+  );
+}
--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:flutter_mvvm/utils/routes/routes_name.dart';
+
+import '../utils/utils.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -19,7 +22,10 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Center(
         child: ElevatedButton(
             onPressed: () {
-              Navigator.pushReplacementNamed(context, RoutesName.home);
+              // Navigator.pushReplacementNamed(context, RoutesName.home);
+              toastMessage('No Internet Connection');
+              flushErrorMessage('No internet', context);
+              showSnackbar('No internet', context);
             },
             child: const Text("Click Me")),
       ),
