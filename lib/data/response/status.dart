4296a9ca070950ff5a7866da3b0d502b6827e0f2
rn-byte <ramthakur977+github@gmail.com>
2024-06-30 16:30:26 +0545

    API response class added

---
+++ lib/data/response/status.dart
@@ -0,0 +1,5 @@
+//An enum (enumeration) is a special data type that allows you to define a set
+//of named constant values. It is useful for representing a fixed number of
+// related values in a type-safe way.
+
+enum Status { LOADING, COMPLETED, ERROR }
