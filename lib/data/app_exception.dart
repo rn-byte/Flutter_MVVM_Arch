3020033d133e4bb0784a6f6d4b5c72f58ca5d5a3
rn-byte <ramthakur977+github@gmail.com>
2024-06-30 15:52:41 +0545

    Exception class added

---
+++ lib/data/app_exception.dart
@@ -0,0 +1,28 @@
+class AppException implements Exception {
+  final _message;
+  final _prefix;
+
+  AppException([this._message, this._prefix]);
+
+  String toString() {
+    return '$_prefix $_message';
+  }
+}
+
+class FetchDataException extends AppException {
+  FetchDataException([String? message])
+      : super(message, 'Error during communication');
+}
+
+class BadRequestException extends AppException {
+  BadRequestException([String? message]) : super(message, 'Invalid Request');
+}
+
+class UnauthorizedException extends AppException {
+  UnauthorizedException([String? message])
+      : super(message, 'Unauthorized Request');
+}
+
+class InvalidInputException extends AppException {
+  InvalidInputException([String? message]) : super(message, 'Invalid Input');
+}
